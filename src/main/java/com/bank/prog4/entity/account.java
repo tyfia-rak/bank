7c5281a621ddc824f6b1601e290ecfbd2b3e4764
Sarobidy <sarobidy.rakotonarivo.1@gmail.com>
2024-03-12 19:13:03 +0300

    feat: add the account model

---
+++ src/main/java/com/bank/prog4/entity/account.java
@@ -0,0 +1,11 @@
+package com.bank.prog4.entity;
+
+import java.sql.Date;
+
+public class account {
+    private String name;
+    private String lastName;
+    private Date dateOfBirth;
+    private Double MonthlySalary;
+    private String accountNumber;
+}
