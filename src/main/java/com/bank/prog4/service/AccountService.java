ec7d98c996c7d0a4bc104b1a1cb7bc90fcf49993
rado <hei.rado.2@gmail.com>
2024-03-19 18:18:55 +0300

    fix: add new service

--- src/main/java/com/bank/prog4/service/AccountService.java
+++ src/main/java/com/bank/prog4/service/AccountService.java
@@ -4,7 +4,9 @@ import com.bank.prog4.DAO.AccountDAO;
 import com.bank.prog4.entity.Account;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.sql.SQLException;
+import java.util.Optional;
 
 @Service
 public class AccountService {
@@ -15,5 +17,8 @@ public class AccountService {
     public Account insert (Account toInsert) throws SQLException{
         accountDAO.save(toInsert);
         return toInsert;
+    }
+    public Account idAccount(int id) throws SQLException{
+        return accountDAO.selectById(id);
     }
 }
