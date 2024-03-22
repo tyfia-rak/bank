8621f018ef9beefd1ab6b4ea4cff6c880935f4de
rado <hei.rado.2@gmail.com>
2024-03-22 14:29:41 +0300

    fix: add all account

--- src/main/java/com/bank/prog4/DAO/AccountDAO.java
+++ src/main/java/com/bank/prog4/DAO/AccountDAO.java
@@ -5,6 +5,7 @@ import com.bank.prog4.Generic.GenericDAO;
 import com.bank.prog4.entity.Account;
 import org.springframework.stereotype.Repository;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -26,12 +27,12 @@ public class AccountDAO implements GenericDAO<Account> {
         try (Connection connection = DatabaseConfig.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setInt(1, toSave.getId());
-            preparedStatement.setString(2,toSave.getFirst_name());
+            preparedStatement.setString(2,toSave.getFirstName());
             preparedStatement.setString(3,toSave.getLastName());
             preparedStatement.setDate(4,toSave.getBirthday());
-            preparedStatement.setDouble(5,toSave.getBank_balance());
-            preparedStatement.setString(6,toSave.getBank_name());
-            preparedStatement.setDouble(7,toSave.getSalary_amount());
+            preparedStatement.setDouble(5,toSave.getBankBalance());
+            preparedStatement.setString(6,toSave.getBankName());
+            preparedStatement.setDouble(7,toSave.getSalaryAmount());
             preparedStatement.setBoolean(8,toSave.getOverdraw());
 
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -76,7 +77,33 @@ public class AccountDAO implements GenericDAO<Account> {
 
     @Override
     public List<Account> findAll() {
-        return null;
+        List<Account> accounts = new ArrayList<>();
+
+        String sql = "SELECT * FROM \"Account\";";
+
+        try (Connection connection = DatabaseConfig.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            while (resultSet.next()) {
+                Account account = new Account();
+                account.setId(resultSet.getInt("id"));
+                account.setFirstName(resultSet.getString("first_name"));
+                account.setLastName(resultSet.getString("last_name"));
+                account.setBirthday(resultSet.getDate("birthday"));
+                account.setBankBalance(resultSet.getDouble("bank_balance"));
+                account.setBankName(resultSet.getString("bank_name"));
+                account.setSalaryAmount(resultSet.getDouble("salary_amount"));
+                account.setOverdraw(resultSet.getBoolean("overdraw"));
+
+                accounts.add(account);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("==> erreur: " + e.getMessage());
+        }
+
+        return accounts;
     }
 
 }
