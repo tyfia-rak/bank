3fc08f8738471fd4b8686d85e0e26479d307359c
rado <hei.rado.2@gmail.com>
2024-03-22 15:25:20 +0300

    fix: update folder Frontend

---
+++ src/Frontend/src/components/ListAccount.js
@@ -0,0 +1,51 @@
+import Table from 'react-bootstrap/Table';
+import axios from 'axios';
+import { useState,useEffect } from 'react';
+function AllAccount (){
+    const [accounts, setAccounts] = useState([]);
+
+    useEffect(() => {
+        fetchAccounts();
+      }, []);
+
+      const fetchAccounts = async () => {
+        try {
+          const response = await axios.get('http://localhost:8080/all_account');
+          setAccounts(response.data);
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };
+
+    return (
+        <Table striped bordered hover variant="white"style={{ width: '50%', marginTop:'10vh' }}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Birthday</th>
+              <th>Bank Balance</th>
+              <th>Bank Name</th>
+              <th>Salary Amount</th>
+              <th>Overdraw</th>
+            </tr>
+          </thead>
+          <tbody>
+          {accounts.map(account => (
+            <tr key={account.id}>
+              <td>{account.id}</td>
+              <td>{account.firstName}</td>
+              <td>{account.lastName}</td>
+              <td>{account.birthday}</td>
+              <td>{account.bankBalance}</td>
+              <td>{account.bankName}</td>
+              <td>{account.salaryAmount}</td>
+              <td>{account.overdraw ? 'Yes' : 'No'}</td>
+            </tr>
+          ))}
+          </tbody>
+        </Table>
+      );   
+}
+export default AllAccount;
---
+++ src/Frontend/src/components/Sidebar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import '../Styles/sidebar.css';
+export default props => {
+  return (
+    <Menu>
+      <a className="menu-item" href="/">
+        Home
+      </a>
+      <a className="menu-item" href="/salads">
+        All Accounts
+      </a>
+      <a className="menu-item" href="/pizzas">
+        Retrait
+      </a>
+      <a className="menu-item" href="/desserts">
+        Transaction
+      </a>
+    </Menu>
+  );
+};
