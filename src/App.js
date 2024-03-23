5947fb8117ded85ee7220e933dd03c415027b16d
rado <hei.rado.2@gmail.com>
2024-03-23 14:45:40 +0300

    fix: add list all account

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
-import Account from './components/account/account';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AllAccount from './components/ListAccount';
+import Account from './components/account';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Account/>
+    <div className="App" id="outer-container">
+      <Header/>
+      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+      <AllAccount/>
+      <Router>
+      <Routes>
+          <Route exact path='/new' Component={Account}/>
+      </Routes>
+    </Router>
+    <Account/>
     </div>
+    
     
   );
 }
