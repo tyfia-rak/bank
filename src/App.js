4071d4e5d4c90b0331b46360dbd8b87753b1a984
rado <hei.rado.2@gmail.com>
2024-03-23 16:24:01 +0300

    feat: add forms for transaction other bank

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
   Route
 } from "react-router-dom";
 import DetailsComponent from './components/DetailsAccount';
+import IncomingTransferForm from './components/TransactionOtherBank';
 
 
 
@@ -26,6 +27,7 @@ function App() {
       </Routes>
     </Router>
     <Account/>
+    <IncomingTransferForm/>
   
     </div>
     
