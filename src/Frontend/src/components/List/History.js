862a9b5564a746afe8b03527597915e076703b50
rado <hei.rado.2@gmail.com>
2024-03-30 19:48:27 +0300

    fix: reorganize frontend folder

---
+++ src/Frontend/src/components/List/History.js
@@ -0,0 +1,12 @@
+import AllRetreat from "../List/ListRetreat";
+import AllTranferMoney from "./ListTranferMoney";
+
+function History(){
+    return(
+        <>
+        <AllRetreat/>
+        <AllTranferMoney/>
+        </>
+    )
+}
+export default History;
