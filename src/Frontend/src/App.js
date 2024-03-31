467f059e66a8866f37c58bc72fb8fbe169b01366
rado <hei.rado.2@gmail.com>
2024-03-31 14:47:32 +0300

    fix: css sidebar

--- src/Frontend/src/App.js
+++ src/Frontend/src/App.js
@@ -1,28 +1,42 @@
 import './App.css';
-import Sidebar from './components/Sidebar';
-import Header from './components/Account/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import AllAccount from './components/Account/ListAccount';
-import Account from './components/Account/account';
+
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
-import DetailsComponent from './components/Account/DetailsAccount';
-import IncomingTransferForm from './components/Transaction_other_bank/TransactionOtherBank';
-import TransferForm from './components/Transaction_between_account/TransactionBetweenAccount';
-import WithdrawForm from './components/Transaction_Retreat/Retreat';
-import AccountForm from './components/Account/InsertAccount';
-import AllRetreat from './components/List/ListRetreat';
-import AllTranferMoney from './components/List/ListTranferMoney';
 
-
+import Home from './components/Home';
+import History from './components/List/History';
+import AllTransaction from './components/Transaction/Transaction';
+import IncomingTransferForm from './components/Transaction/Transaction_other_bank/TransactionOtherBank';
+ 
 
 function App() {
   return (
     <div className="App" id="outer-container">
-      <Header/>
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/transaction" element={<AllTransaction />} />
+        <Route path="/Approvisionment" element={<IncomingTransferForm />} />
+
+
+      </Routes>
+    </Router>
+  </div>
+    
+    
+  );
+}
+
+export default App;
+/**
+ * <Header/>
       <DetailsComponent/>
       <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
       <AllAccount/>
@@ -37,10 +51,4 @@ function App() {
     <WithdrawForm/>
     <AllRetreat/>
     <AllTranferMoney/>
-    </div>
-    
-    
-  );
-}
-
-export default App;
+ */
